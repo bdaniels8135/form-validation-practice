a011b521180f626195ddc92a958f4e70b28752e7
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-22 11:20:49 -0500

    Create a postal code validation list

--- src/index.js
+++ src/index.js
@@ -7,25 +7,79 @@ import {
   wrapHtmlElements,
 } from "./htmlBuilders";
 
-const countriesList = [
-  "Australia",
-  "Austria",
-  "Belgium",
-  "Brazil",
-  "Canada",
-  "Denmark",
-  "Holland",
-  "Germany",
-  "Hungary",
-  "Italy",
-  "Japan",
-  "Luxembourg",
-  "Poland",
-  "Spain",
-  "Sweden",
-  "United Kingdom",
-  "United States",
+const countryPostalCodeValidations = [
+  {
+    country: "Australia",
+    regex: /[0-9]{4}/,
+  },
+  {
+    country: "Austria",
+    regex: /[0-9]{4}/,
+  },
+  {
+    country: "Belgium",
+    regex: /[0-9]{4}/,
+  },
+  {
+    country: "Brazil",
+    regex: /[0-9]{5}[-]?[0-9]{3}/,
+  },
+  {
+    country: "Canada",
+    regex: /[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]/,
+  },
+  {
+    country: "Denmark",
+    regex: /[0-9]{3,4}/,
+  },
+  {
+    country: "Holland",
+    regex: /[1-9][0-9]{3}\s?[a-zA-Z]{2}/,
+  },
+  {
+    country: "Germany",
+    regex: /[0-9]{5}/,
+  },
+  {
+    country: "Hungary",
+    regex: /[0-9]{4}/,
+  },
+  {
+    country: "Italy",
+    regex: /[0-9]{5}/,
+  },
+  {
+    country: "Japan",
+    regex: /\d{3}-\d{4}/,
+  },
+  {
+    country: "Luxembourg",
+    regex: /(L\s*(-|—|–))\s*?[\d]{4}/,
+  },
+  {
+    country: "Poland",
+    regex: /[0-9]{2}-[0-9]{3}/,
+  },
+  {
+    country: "Spain",
+    regex: /((0[1-9]|5[0-2])|[1-4][0-9])[0-9]{3}/,
+  },
+  {
+    country: "Sweden",
+    regex: /\d{3}\s?\d{2}/,
+  },
+  {
+    country: "United Kingdom",
+    regex:
+      /[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}/,
+  },
+  {
+    country: "United States",
+    regex: /(\d{5}([-]\d{4})?)/,
+  },
 ];
+
+const countriesList = countryPostalCodeValidations.map((item) => item.country);
 
 const formInputsList = [
   {
